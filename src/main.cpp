2c0125c6ec48720995e624f345d26162ded8ee8c
Aman Singh <amansingh81757@gmail.com>
2024-06-23 23:55:09 +0530

    handling missing commands2

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,6 @@ int main() {
 
   std::string input;
   std::getline(std::cin, input);
-  std::cout<< input << ": command not found";
+  std::cout<< input << ": command not found\n";
   std::getline(std::cin, input);
 }
