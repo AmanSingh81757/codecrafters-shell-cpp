29d0c81b15438a98e112d752fc7c52bb56b51f0d
Aman Singh <amansingh81757@gmail.com>
2024-06-24 00:07:15 +0530

    implementing type builtins

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,12 @@ int main() {
     std::getline(std::cin, input);
     if(input == "exit 0") break;
     if(input.substr(0, 4) == "echo") std::cout << input.substr(5) << "\n";
+    if(input.substr(0, 4) == "type") {
+      if(input.substr(5) == "echo") std::cout << "echo is a shell builtin\n";
+      if(input.substr(5) == "exit") std::cout << "exit is a shell builtin\n";
+      if(input.substr(5) == "type") std::cout << "type is a shell builtin\n";
+      else std::cout << input.substr(5) << ": command not found\n";
+    }
     else std::cout << input << ": command not found\n";
     std::cout << "$ ";
   }
