21602169aa80825ab0f04521c67f42661f5bdac3
Aman Singh <amansingh81757@gmail.com>
2024-06-26 14:41:21 +0530

    executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <sstream>
+#include <filesystem>
+using namespace std;
+
+std::string get_path(std::string command){
+    std::string path_env = std::getenv("PATH");
+    std::stringstream ss(path_env);
+    std::string path;
+    while(!ss.eof()){
+        getline(ss, path, ':');
+        string abs_path = path + '/' + command;
+        if(filesystem::exists(abs_path)){
+            return abs_path;
+        }
+    }
+    return "";
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -19,11 +36,14 @@ int main() {
     if(input.substr(0, 4) == "type") {
       std::string user_input = input.substr(5);
       if(user_input == "echo" || user_input == "type" || user_input == "exit") std::cout << user_input << " is a shell builtin\n";
-      else std::cout << input.substr(5) << ": not found\n";
+      else {
+        std::string path = get_path(user_input);
+        if(path == "") std::cout << user_input << " not found\n";
+        else std::cout << user_input << " is " << path << "\n";
+      }
       std::cout << "$ ";
       continue;
     }
-    else std::cout << input << ": command not found\n";
     std::cout << "$ ";
   }
 }
